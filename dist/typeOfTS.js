a0ed9d0c0ae0937b90dfa0531ba0809f38a3d4b9
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-02 22:25:21 +0600

    basic installition of TS

---
+++ dist/typeOfTS.js
@@ -0,0 +1,4 @@
+"use strict";
+const course = 'next level web development I am ready';
+const courseUser = 'Din Islam rajin yo yo khaba naki ';
+console.log(course, courseUser);
---
+++ src/typeOfTS.ts
@@ -0,0 +1,3 @@
+const course: string = 'next level web development I am ready'
+const courseUser: string = 'Din Islam rajin yo yo khaba naki '
+console.log(course, courseUser);
