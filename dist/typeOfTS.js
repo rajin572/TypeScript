1b8529d2b8bf77ad42bc1111dbb76be3f2d1c988
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-03 18:19:50 +0600

    learn object

--- dist/typeOfTS.js
+++ dist/typeOfTS.js
@@ -1,4 +1,5 @@
 "use strict";
 const course = 'next level web development I am ready';
 const courseUser = 'Din Islam rajin yo yo khaba naki ';
+let con;
 console.log(course, courseUser);
--- src/typeOfTS.ts
+++ src/typeOfTS.ts
@@ -1,3 +1,31 @@
+// write code
 const course: string = 'next level web development I am ready'
 const courseUser: string = 'Din Islam rajin yo yo khaba naki '
 console.log(course, courseUser);
+
+// Array
+let names: string[] = ['dada', 'nana']
+
+// Touple
+
+let data: [number, string] = [1, 'sampoo']
+
+// object
+const users :{
+    readonly student: boolean
+    university: 'Kabi Nazrul'
+    name: string,
+    id: number,
+    phone: number,
+    relation: boolean
+    partner?: string
+} = {
+    university: 'Kabi Nazrul',
+    name:"Din Islam Rajin",
+    student: true,
+    id: 5,
+    phone: 1647742754,
+    relation: true,
+    partner: 'mim'
+}
+// users.student = 5
