2f00d40bc1fdc7c3cdd8903176167253b49c9993
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-05 00:32:57 +0600

    publish

---
+++ dist/unionAndInterjectin.js
@@ -0,0 +1,15 @@
+"use strict";
+var Level;
+(function (Level) {
+    Level["junior"] = "junior";
+    Level["senior"] = "senior";
+    Level["mid"] = "mid";
+})(Level || (Level = {}));
+const developer = {
+    name: 'rajin',
+    position: 'web developer',
+    expertise: 'react',
+    experience: 5,
+    // level: Level.junior
+    level: 'senior'
+};
