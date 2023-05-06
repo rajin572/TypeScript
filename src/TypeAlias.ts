d80e0a0a2b750c326e77d4487e89a14a9abe8038
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-06 13:38:42 +0600

    added

---
+++ src/TypeAlias.ts
@@ -0,0 +1,41 @@
+//Type alias is used to defind the type 
+// we can defind types for object, array, string, function etc.
+
+// use type alias in object
+
+type customeParants = {
+    fatherName: string,
+    motherName: string,
+    numberOfSiblings: number,
+    siblingsName?: string[]
+}
+
+type customePeople = {
+    name: string,
+    age: number,
+    phone: number,
+    parants: customeParants
+}
+
+const people: customePeople = {
+    name: 'rajin',
+    age: 22,
+    phone: 8801647742754,
+    parants:{
+        fatherName: 'hasu',
+        motherName: 'poly',
+        numberOfSiblings: 1,
+        siblingsName: ['siyam']
+    }
+}
+
+// type alias in function
+
+
+type oparationType = (x:number,y:number) => number 
+
+const calculator = (num1:number, num2:number, oparation : oparationType) => {
+    return oparation(num1, num2)
+}
+
+console.log(calculator(10, 20, (x,y) => x+y));
