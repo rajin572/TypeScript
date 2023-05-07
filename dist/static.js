2f1d9654d8dbf1d63e31e5fc6948777bc36ffcb1
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-08 00:38:14 +0600

    static

--- dist/static.js
+++ dist/static.js
@@ -1,1 +1,13 @@
 "use strict";
+class Counter {
+    static increment() {
+        return Counter.counter = Counter.counter + 1;
+    }
+    static decrement() {
+        return Counter.counter = Counter.counter - 1;
+    }
+}
+Counter.counter = 0;
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
