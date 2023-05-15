0a96f4aa778abe5e2e7bbef23867d1aa2d78e3a1
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-15 23:00:49 +0600

    import and export

---
+++ src/importAndExport.ts
@@ -0,0 +1,17 @@
+// import Jekono from "./module"; // default export import
+// import add as addtwo from './utils/add'
+// import multiply from './utils/multiply'
+// import average from './utils/average'
+// import methods from "./utils/index"; // import like as a object
+
+
+
+
+// default export____________________________
+
+// export default {
+//   add,
+//   subtract,
+//   multiply,
+//   average,
+// };
